e3f7e6132c5e50cf05e305d4fe8722f679aa40b0
Airtox <kylou2904@gmail.com>
2024-03-19 19:00:55 +0100

    init input

--- ConsoleApp1/ConsoleApp1/Game/Manager/InputManager.cs
+++ ConsoleApp1/ConsoleApp1/Game/Manager/InputManager.cs
@@ -1,10 +1,20 @@
-﻿public class InputManager
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class InputManager
 {
-    public int PressA()
-    { 
-        
+    void Update()
+    {
+        if (Console.ReadKey(ConsoleKey.UpArrow))
+        {
+            Console.WriteLine("up arrow key is held down");
+        }
 
-
-        return 0;
+        if (Console.ReadKey('a'))
+        {
+           Console.WriteLine("down arrow key is held down");
+        }
     }
 }
