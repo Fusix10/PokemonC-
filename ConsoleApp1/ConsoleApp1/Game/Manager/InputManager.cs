03f17dd0a349164ed78fbb92f88a228e7f5a5328
Airtox <117281228+KyllianT@users.noreply.github.com>
2024-03-20 11:23:01 +0100

    Update InputManager.cs
    finish input

--- ConsoleApp1/ConsoleApp1/Game/Manager/InputManager.cs
+++ ConsoleApp1/ConsoleApp1/Game/Manager/InputManager.cs
@@ -1,20 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-public class InputManager
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
 {
-    void Update()
+    private static InputManager instance;
+
+    public static InputManager Instance
     {
-        if (Console.ReadKey(ConsoleKey.UpArrow))
+        get
         {
-            Console.WriteLine("up arrow key is held down");
+            if (instance == null)
+            {
+                instance = FindObjectOfType<InputManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(InputManager).Name;
+                    instance = obj.AddComponent<InputManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private Hashtable keyBindings = new Hashtable();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
         }
 
-        if (Console.ReadKey('a'))
+        InitializeKeyBindings();
+    }
+
+    private void InitializeKeyBindings()
+    {
+        // Ajoutez les touches et leurs fonctions associées ici
+        AddKeyBinding(KeyCode.UpArrow, OnUpPressed);
+        AddKeyBinding(KeyCode.DownArrow, OnDownPressed);
+        AddKeyBinding(KeyCode.LeftArrow, OnLeftPressed);
+        AddKeyBinding(KeyCode.RightArrow, OnRightPressed);
+        AddKeyBinding(KeyCode.A, OnAPressed);
+        AddKeyBinding(KeyCode.Z, OnZPressed);
+        AddKeyBinding(KeyCode.E, OnEPressed);
+        AddKeyBinding(KeyCode.R, OnRPressed);
+        AddKeyBinding(KeyCode.Tab, OnTabPressed);
+    }
+
+    private void AddKeyBinding(KeyCode keyCode, Action action)
+    {
+        if (!keyBindings.ContainsKey(keyCode))
         {
-           Console.WriteLine("down arrow key is held down");
+            keyBindings.Add(keyCode, action);
         }
+        else
+        {
+            keyBindings[keyCode] = action;
+        }
+    }
+
+    private void Update()
+    {
+        foreach (KeyCode keyCode in keyBindings.Keys)
+        {
+            if (Input.GetKeyDown(keyCode))
+            {
+                Action action = (Action)keyBindings[keyCode];
+                action?.Invoke();
+            }
+        }
+    }
+
+    // Fonctions appelées lorsque les touches sont pressées
+    private void OnUpPressed()
+    {
+        Debug.Log("Up Arrow Pressed");
+    }
+
+    private void OnDownPressed()
+    {
+        Debug.Log("Down Arrow Pressed");
+    }
+
+    private void OnLeftPressed()
+    {
+        Debug.Log("Left Arrow Pressed");
+    }
+
+    private void OnRightPressed()
+    {
+        Debug.Log("Right Arrow Pressed");
+    }
+
+    private void OnAPressed()
+    {
+        Debug.Log("A Pressed");
+    }
+
+    private void OnZPressed()
+    {
+        Debug.Log("Z Pressed");
+    }
+
+    private void OnEPressed()
+    {
+        Debug.Log("E Pressed");
+    }
+
+    private void OnRPressed()
+    {
+        Debug.Log("R Pressed");
+    }
+
+    private void OnTabPressed()
+    {
+        Debug.Log("Tab Pressed");
     }
 }
