9a6ee9e3c9d803d1120412bacb3ea2d5c2860ce2
Airtox <kylou2904@gmail.com>
2024-03-20 14:40:02 +0100

    bug showInputManager

--- ConsoleApp1/ConsoleApp1/Game/Manager/InputManager.cs
+++ ConsoleApp1/ConsoleApp1/Game/Manager/InputManager.cs
@@ -6,112 +6,116 @@ public class InputManager
 
     private Hashtable keyBindings = new Hashtable();
 
-    public void Awake(object gameObject)
-    {
-        if (instance == null)
+    public void ShowInputManager() {
+
+        void Awake(object gameObject)
         {
-            instance = this;
-        }
-        else if (instance != this)
-        {
-            Destroy(gameObject);
-        }
-
-        InitializeKeyBindings();
-    }
-
-    public void Destroy(object gameObject)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void InitializeKeyBindings()
-    {
-        // Ajoutez les touches et leurs fonctions associées ici
-        AddKeyBinding(ConsoleKey.UpArrow, OnUpPressed);
-        AddKeyBinding(ConsoleKey.DownArrow, OnDownPressed);
-        AddKeyBinding(ConsoleKey.LeftArrow, OnLeftPressed);
-        AddKeyBinding(ConsoleKey.RightArrow, OnRightPressed);
-        AddKeyBinding(ConsoleKey.A, OnAPressed);
-        AddKeyBinding(ConsoleKey.Z, OnZPressed);
-        AddKeyBinding(ConsoleKey.E, OnEPressed);
-        AddKeyBinding(ConsoleKey.R, OnRPressed);
-        AddKeyBinding(ConsoleKey.Tab, OnTabPressed);
-    }
-
-    public void AddKeyBinding(ConsoleKey keyCode, Action action)
-    {
-        if (!keyBindings.ContainsKey(keyCode))
-        {
-            keyBindings.Add(keyCode, action);
-        }
-        else
-        {
-            keyBindings[keyCode] = action;
-        }
-    }
-
-    public void Update(Action? action)
-    {
-        if (Console.KeyAvailable)
-        {
-            var keys = Console.ReadKey(true).Key;
-
-            foreach (ConsoleKey keyCode in keyBindings.Keys)
+            if (instance == null)
             {
-                if (keys == keyCode)
-                {
-                    Action? action1 = keyBindings[keyCode] as Action;
-                    action?.Invoke();
-                }
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            InitializeKeyBindings();
+        }
+
+        void Destroy(object gameObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        void InitializeKeyBindings()
+        {
+            // Ajoutez les touches et leurs fonctions associées ici
+            AddKeyBinding(ConsoleKey.UpArrow, OnUpPressed);
+            AddKeyBinding(ConsoleKey.DownArrow, OnDownPressed);
+            AddKeyBinding(ConsoleKey.LeftArrow, OnLeftPressed);
+            AddKeyBinding(ConsoleKey.RightArrow, OnRightPressed);
+            AddKeyBinding(ConsoleKey.A, OnAPressed);
+            AddKeyBinding(ConsoleKey.Z, OnZPressed);
+            AddKeyBinding(ConsoleKey.E, OnEPressed);
+            AddKeyBinding(ConsoleKey.R, OnRPressed);
+            AddKeyBinding(ConsoleKey.Tab, OnTabPressed);
+        }
+
+        void AddKeyBinding(ConsoleKey keyCode, Action action)
+        {
+            if (!keyBindings.ContainsKey(keyCode))
+            {
+                keyBindings.Add(keyCode, action);
+            }
+            else
+            {
+                keyBindings[keyCode] = action;
             }
         }
 
-    }
+        void Update(Action? action)
+        {
+            if (Console.KeyAvailable)
+            {
+                var keys = Console.ReadKey(true).Key;
 
-    // Fonctions appelées lorsque les touches sont pressées
-    private void OnUpPressed()
-    {
-        Console.WriteLine("Up Arrow Pressed");
-    }
+                foreach (ConsoleKey keyCode in keyBindings.Keys)
+                {
+                    if (keys == keyCode)
+                    {
+                        Action? action1 = keyBindings[keyCode] as Action;
+                        action?.Invoke();
+                    }
+                }
+            }
 
-    private void OnDownPressed()
-    {
-        Console.WriteLine("Down Arrow Pressed");
-    }
+        }
 
-    private void OnLeftPressed()
-    {
-        Console.WriteLine("Left Arrow Pressed");
-    }
+        // Fonctions appelées lorsque les touches sont pressées
+        void OnUpPressed()
+        {
+            Console.WriteLine("Up Arrow Pressed");
+        }
 
-    private void OnRightPressed()
-    {
-        Console.WriteLine("Right Arrow Pressed");
-    }
+        void OnDownPressed()
+        {
+            Console.WriteLine("Down Arrow Pressed");
+        }
 
-    private void OnAPressed()
-    {
-        Console.WriteLine("A Pressed");
-    }
+        void OnLeftPressed()
+        {
+            Console.WriteLine("Left Arrow Pressed");
+        }
 
-    private void OnZPressed()
-    {
-        Console.WriteLine("Z Pressed");
-    }
+        void OnRightPressed()
+        {
+            Console.WriteLine("Right Arrow Pressed");
+        }
 
-    private void OnEPressed()
-    {
-        Console.WriteLine("E Pressed");
-    }
+        void OnAPressed()
+        {
+            Console.WriteLine("A Pressed");
+        }
 
-    private void OnRPressed()
-    {
-        Console.WriteLine("R Pressed");
-    }
+        void OnZPressed()
+        {
+            Console.WriteLine("Z Pressed");
+        }
 
-    private void OnTabPressed()
-    {
-        Console.WriteLine("Tab Pressed");
+        void OnEPressed()
+        {
+            Console.WriteLine("E Pressed");
+        }
+
+        void OnRPressed()
+        {
+            Console.WriteLine("R Pressed");
+        }
+
+        void OnTabPressed()
+        {
+            Console.WriteLine("Tab Pressed");
+        }
+
     }
 }
--- ConsoleApp1/ConsoleApp1/Game/Program.cs
+++ ConsoleApp1/ConsoleApp1/Game/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using static System.Collections.Specialized.BitVector32;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
+      InputManager inputManager = new InputManager();
+        inputManager.ShowInputManager();
     }
 }
