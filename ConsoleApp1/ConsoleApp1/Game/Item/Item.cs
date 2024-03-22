802aac874f0c814b45aafd1cec8e32d53822299b
Airtox <kylou2904@gmail.com>
2024-03-22 11:25:12 +0100

    in comment TODO

--- ConsoleApp1/ConsoleApp1/Game/Item/Item.cs
+++ ConsoleApp1/ConsoleApp1/Game/Item/Item.cs
@@ -1,6 +1,46 @@
 ﻿public class Item
 {
+    // creat item : potionNormal/potionMax/upCapacityAtt/upCapacityDef/upCapacitySpeed/candyXp/pokeBall/hyperBall/SuperBall/masterBall
 
+
+
+    //potionNormal : Available = rand (1 to 20) / Heal = 20 / hpPlayer = hpPokemon + Heal
+
+
+
+    //potionMax : Available = rand (1 to 10) / Heal = 50 / hpPlayer = hpPokemon + Heal
+
+
+
+    //upCapacityAtt : Available = rand (1 to 5) / bosstAtt = 20 / attackPokemon = attackPokemon + bosstAtt 
+
+
+
+    //upCapacityDef : Available = rand (1 to 5) / bosstDef = 20 / defencePokemon = defencePokemon + bosstDef
+
+
+
+    //upCapacitySpeed : Available = rand (1 to 5) / bosstSpeed = 20 / speedPokemon = speedPokemon + bosstSpeed
+
+
+
+    //candyXp : Available = rand (1 to 15) / bonusXp = 100 / xpPokemon = xpPokemon + bonusXp
+
+
+
+    //pokeBall :Available = rand (25 to 50) / bonusCatch = 0% / catchPokemon = catchPokemon + bonusCatch
+
+
+
+    //superBall : Available = rand (20 to 40) / bonusCatch = 10% / catchPokemon = catchPokemon + bonusCatch
+
+
+
+    //hyperBall : Available = rand (5 to 15) / bonusCatch = 20% / catchPokemon = catchPokemon + bonusCatch
+
+
+
+    //masterBall : Available = 1 / bonusCatch = 100% / catchPokemon = catchPokemon + bonusCatch
 
 
 }
