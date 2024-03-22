409e359143e5f54c897fdb259f6f67c99a95a9a2
William-Gabali <williamgabali@gmail.com>
2024-03-22 12:19:51 +0100

    update menu / menu fight

--- ConsoleApp1/ConsoleApp1/Game/Menu/MenuFigth.cs
+++ ConsoleApp1/ConsoleApp1/Game/Menu/MenuFigth.cs
@@ -1,30 +1,61 @@
 ﻿using System;
 
-
-class MenuCombat
+class MenuFigth  
 {
-    public void StartCombat()
+    public void ShowMenuFigth()
     {
+        bool quitGameFight = false;
 
-        Pokemon[,] tableauEchecs = new Pokemon[8,8];
+        while (!quitGameFight)
+        {
+            Console.WriteLine("\r\n░▒▓████████▓▒░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ \r\n░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░     \r\n░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░     \r\n░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒▒▓███▓▒░▒▓████████▓▒░  ░▒▓█▓▒░     \r\n░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░     \r\n░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░     \r\n░▒▓█▓▒░      ░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░     \r\n                                                            \r\n                                                            \r\n");
+            Console.WriteLine("=== Menu Fight ===");
+            Console.WriteLine("1. Dépacement");
+            Console.WriteLine("2. Attack");
+            Console.WriteLine("3. Bag");
+            Console.WriteLine("4. Prendre la Fuite");
+            Console.Write("Please select an option: ");
 
-        InitialiseTableauEchecs(tableauEchecs);
+            string choice = Console.ReadLine();
 
-        AfficherTableauEchecs(tableauEchecs);
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("=== choisisé le pokemon qui va se deplacé ===");
+                    Console.WriteLine("1. Poke 1");
+                    Console.WriteLine("2. Poke 2");
+                    Console.WriteLine("3. Poke 3");
+                    Console.WriteLine("4. Poke 4");
+                    // Code to create a new save and player
+                    break;
+                case "2":
+                    Console.WriteLine("=== Quelle attack vous voulez utilisé ===");
+                    Console.WriteLine("1. Atack 1");
+                    Console.WriteLine("2. Atack 2");
+                    Console.WriteLine("3. Atack 3");
+                    Console.WriteLine("4. Atack 4");
+                    // Code to load saved game
+                    break;
+                case "3":
+                    Console.WriteLine("=== Quel object souhaité vous utilisé ===");
+                    Console.WriteLine("1. Object 1");
+                    Console.WriteLine("2. Object 2");
+                    Console.WriteLine("3. Object 3");
+                    Console.WriteLine("4. Object 4");
+                    //
+                    break;
+                case "4":
+                    Console.WriteLine(" ratio + cheh + Gros Noobs");
+                    quitGameFight = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
 
+            Console.WriteLine(); // Just for spacing
+        }
     }
 
-    private void InitialiseTableauEchecs(Pokemon[,] tableau)
-    {
-        
-    }
-    private void AfficherTableauEchecs(Pokemon[,] tableau)
-    {
-       
-    }
 
-    internal void ShowmenuFigth()
-    {
-        
-    }
-} 
+}
